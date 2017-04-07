95c9d6468185094cefdc7635bcad8025c6419591
fiatjaf <fiatjaf@gmail.com>
2017-04-07 22:54:43 +0000

    after 2-days fight things seem to be finally settled.

--- main.go
+++ main.go
@@ -2,14 +2,36 @@ package main
 
 import (
 	"github.com/fiatjaf/summadb/database"
+	"github.com/fiatjaf/summadb/types"
 	"github.com/gopherjs/gopherjs/js"
 )
 
 func main() {
-	js.Module.Set("exports", NewDatabase)
-}
+	js.Module.Set("exports", func(name string, adapter string) map[string]interface{} {
+		summa := database.Open(name, adapter)
+		path := types.Path{}
 
-func NewDatabase(name string, adapter string) *database.SummaDB {
-	db := database.Open(name, adapter)
-	return db
+		return map[string]interface{}{
+			"set": func(doc map[string]interface{}) *js.Object {
+				return js.Global.Get("Promise").New(func(resolve, reject *js.Object) {
+					go func() {
+						tree := types.TreeFromInterface(doc)
+						resolve.Invoke(summa.Set(path, tree))
+					}()
+				})
+			},
+			"read": func() *js.Object {
+				return js.Global.Get("Promise").New(func(resolve, reject *js.Object) {
+					go func() {
+						tree, err := summa.Read(path)
+						if err != nil {
+							reject.Invoke(err)
+							return
+						}
+						resolve.Invoke(tree.ToInterface())
+					}()
+				})
+			},
+		}
+	})
 }
--- test.js
+++ test.js
@@ -1,10 +1,17 @@
-const tape = require('tape')
+const tape = require('blue-tape')
 const SummulaDB = require('./summuladb')
 
 const db = SummulaDB('something', 'memdown')
 
 tape('basic', function (t) {
-  t.plan(1)
   t.ok(db, 'db was created.')
+  t.equals(typeof db.set, 'function', '.set is a function that exists')
+  t.equals(typeof db.read, 'function', '.read is a function that exists')
+  return db.set({hello: 'world'})
+    .then(() => db.read())
+    .then(doc => {
+      t.equal(doc._rev.slice(0, 2), '1-', 'top level _rev')
+      t.equal(doc.hello._rev.slice(0, 2), '1-', 'nested _rev')
+      t.equal(doc.hello._val, 'world', 'value')
+    })
 })
-
